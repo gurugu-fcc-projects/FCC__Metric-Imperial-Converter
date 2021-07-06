c6135da358c8337ec7420f3bc4aa09deebd9db85
GuRuGu <peter.k.nets@gmail.com>
2021-07-06 17:52:14 +0300

    Resolve tests for reading a whole or a decimal number

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,43 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    // let result = input.match(/^\d+\.?\d+//?\d+/);
+    let result = input.match(/^\d+\.?\d*(\/\d+)?/);
+    console.log(result);
+    return result[0];
   };
-  
-  this.getUnit = function(input) {
+
+  this.getUnit = function (input) {
     let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
