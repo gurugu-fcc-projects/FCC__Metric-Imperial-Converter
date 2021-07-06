682c5ee11476ef5c33ae3c37e49609f85f18d64d
GuRuGu <peter.k.nets@gmail.com>
2021-07-06 17:53:27 +0300

    Add unit test for reading a fractional input

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,19 +1,34 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-  
-  test('convertHandler should correctly read a whole number input', function() {
-    const result = convertHandler.getNum('12mi');
-    assert.equal(result, 12, 'Given input of "12mi" should return a whole number "12"');
+suite("Unit Tests", function () {
+  test("convertHandler should correctly read a whole number input", function () {
+    const result = convertHandler.getNum("12mi");
+    assert.equal(
+      result,
+      12,
+      'Given input of "12mi" should return a whole number "12"'
+    );
   });
 
-  test('convertHandler should correctly read a decimal number input', function() {
-    const result = convertHandler.getNum('12.2mi');
-    assert.equal(result, 12.2, 'Given input of "12.2mi" should return a decimal number "12.2"');
+  test("convertHandler should correctly read a decimal number input", function () {
+    const result = convertHandler.getNum("12.2mi");
+    assert.equal(
+      result,
+      12.2,
+      'Given input of "12.2mi" should return a decimal number "12.2"'
+    );
   });
 
+  test("convertHandler should correctly read a fractional input", function () {
+    const result = convertHandler.getNum("1/2mi");
+    assert.equal(
+      result,
+      "1/2",
+      'Given input of "1/2mi" should return a fractional number "1/2"'
+    );
+  });
 });
