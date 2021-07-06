ab5e75f87c4bb0b9d8ea2e9cd7b4e5bd0a137a4d
GuRuGu <peter.k.nets@gmail.com>
2021-07-06 18:06:18 +0300

    Look for a split point

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  let splitIdx = null;
+
+  for (let i = 0; i <= input.length; i++) {
+    const match = input.charAt(i).toLowerCase().match(/[a-z]/);
+
+    if (match) {
+      splitIdx = i;
+      break;
+    }
+  }
 };
