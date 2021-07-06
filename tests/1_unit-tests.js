8724b1e657e430edbb7e4779d2c8a6503146b9ac
GuRuGu <peter.k.nets@gmail.com>
2021-07-06 17:51:23 +0300

    Add unit tests for reading a whole or a decimal number

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,15 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+  
+  test('convertHandler should correctly read a whole number input', function() {
+    const result = convertHandler.getNum('12mi');
+    assert.equal(result, 12, 'Given input of "12mi" should return a whole number "12"');
+  });
+
+  test('convertHandler should correctly read a decimal number input', function() {
+    const result = convertHandler.getNum('12.2mi');
+    assert.equal(result, 12.2, 'Given input of "12.2mi" should return a decimal number "12.2"');
+  });
 
 });
