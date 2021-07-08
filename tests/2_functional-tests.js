f9e51d19c6b8156b6e23cd64f0eeea5f4c7cd987
GuRuGu <peter.k.nets@gmail.com>
2021-07-08 15:32:42 +0300

    Add a functional test for converting 10L

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,19 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 let assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite("Functional Tests", function () {
+  test("Convert a valid input such as 10L: GET request to /api/convert", function (done) {
+    chai
+      .request(server)
+      .get("/api/convert?input=10L")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "10 liters converts to 2.64172 gallons");
+        done();
+      });
+  });
 });
